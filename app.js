dd9f9d277f909c1778379af788a93d5ce432a685
leo <leozhang621@gmail.com>
2018-01-10 16:16:29 +0800

    完成post

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const bodyParser = require('body-parser')
 
 const server = app.listen(3000,function(){
     let port = server.address().port
@@ -10,14 +11,23 @@ const server = app.listen(3000,function(){
 app.use(express.static('lib'))
 app.use(express.static('images'))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extend:true}))
+
 app.get('/',function(req,res){
     res.sendfile(__dirname + '/index.html')
 })
 
+//get请求
 app.get('/personal',function(req,res){
     fs.readFile('./data.json','utf8',(err,data)=>{
         res.send(data)
     })
+})
+
+//post请求
+app.post('/post_data',function(req,res){
+    console.log(req.body)
 })
 
 
